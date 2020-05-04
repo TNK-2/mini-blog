8ea2464e7dbf835ea91ae21a9deb3a1d11bcde20
石塚　祐輔 <ishizukayusuke@LAB-N1845.local>
2020-05-04 11:17:33 +0900

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  h1 Hello world.
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+// 環境変数の参照サンプル
+console.log('process.env.envName:', process.env.envName);
+console.log('process.env.apiEndpointUrl:', process.env.apiEndpointUrl);
+
+@Component
+export default class IndexPage extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
